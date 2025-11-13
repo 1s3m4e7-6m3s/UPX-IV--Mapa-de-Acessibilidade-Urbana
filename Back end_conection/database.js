74a82d729590a899a446658ec95b93162730b880
Ismael-G0m3s <ismaelgomesalvez@outlook.com>
2025-11-13 20:42:21 -0300

    Connecção ao SQL Sever
    Implementação de configurações iniciais para conecção do SQL Server .

---
+++ Back end_conection/database.js
@@ -0,0 +1,32 @@
+const sql = require('mssql');
+
+//Configuração para Windowns Authentication
+const config = {
+    sever: "DESKTIO-B4JK7O0\\SQLEXPRESS",
+    database: "Acessmap_DB",
+    options: {
+        encrypt: false,
+        trustSeverCertificate: true
+    },
+    authentication: {
+        type: "ntlm",
+        options: {
+            domain: "",
+            userName: "",
+            passWord: ""
+        }
+    }
+};
+
+async function getConnection() {
+    try {
+        const pool = await sql.connect(confing);
+        console.log("Conectado ao SQL Server com sucesso!");
+        return pool;
+    } catch (error) {
+        console.error("Algo deu errado ao tentar conectar com SQL Sever: ")
+        throw error;
+    }
+}
+
+module.exports = {getConnection};
