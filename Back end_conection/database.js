263b1ebd1fd6ccc285b04e35122466f2abff3329
Ismael-G0m3s <ismaelgomesalvez@outlook.com>
2025-11-16 16:21:57 -0300

    Update database.js

--- Back end_conection/database.js
+++ Back end_conection/database.js
@@ -2,29 +2,30 @@ const sql = require('mssql');
 
 //Configuração para Windowns Authentication
 const config = {
-    sever: "DESKTOP-B4JK7O0\\SQLEXPRESS",
+    server: "DESKTOP-B4JK7O0\\SQLEXPRESS",
     database: "Acessmap_DB",
     options: {
         encrypt: false,
-        trustSeverCertificate: true
+        trustServerCertificate: true,
+        enableArithAbort: true
     },
     authentication: {
         type: "ntlm",
         options: {
             domain: "",
             userName: "",
-            passWord: ""
+            password: ""
         }
     }
 };
 
 async function getConnection() {
     try {
-        const pool = await sql.connect(confing);
+        const pool = await sql.connect(config);
         console.log("Conectado ao SQL Server com sucesso!");
         return pool;
     } catch (error) {
-        console.error("Algo deu errado ao tentar conectar com SQL Sever: ")
+        console.error("Algo deu errado ao tentar conectar com SQL Sever: ", error)
         throw error;
     }
 }
